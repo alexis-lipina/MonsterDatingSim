a5df530b83f0be4ed3cf981e4b5c4b5b14463b8d
Mark <mal4739@g.rit.edu>
2017-10-16 00:03:55 -0400

    Trying to commit the same thing again...

---
+++ Assets/InteractibleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleScript : MonoBehaviour {
+
+    public NPCDialogue testobj;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown ()
+    {
+        print("Clicked!");
+        testobj.StartDialogue();
+    }
+}
---
+++ Assets/PortalScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+    public string DestinationScene;
+    public LevelManager LevMan;
+
+    void OnMouseDown()
+    {
+        print("Portal clicked!");
+        LevMan.ChangeScene(DestinationScene);
+    }
+
+
+}
